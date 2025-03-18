f7709a5dee0f3c37fe677a127358d3942c2505ad
Sanket Naik <sanketnaik288@gmail.com>
2025-03-18 16:44:10 +0530

    Second Commit

--- src/main/java/com/example/test3/Test3Application.java
+++ src/main/java/com/example/test3/Test3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test3Application {
-
+	int i=0;
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
 	}
